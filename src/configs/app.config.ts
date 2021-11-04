aac506420cfcba494f3bce8f7dc3da8a5b8ea310
gerpan <nhaths4701@gmail.com>
2021-11-04 16:44:21 +0700

    Update PORT

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -5,7 +5,7 @@ export interface IAppConfig {
 }
 
 export default (): IAppConfig => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10) || 4000,
   bcryptSalt: parseInt(process.env.BCRYPT_SALT, 10) || 10,
   apiPrefix: process.env.API_PREFIX || 'api',
 });
