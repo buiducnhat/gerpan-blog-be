61f49fe34df75378c2d38736e5b1daf2b247c5ef
gerpan <nhaths4701@gmail.com>
2021-11-09 21:47:35 +0700

    Update dto for article and comment

--- src/modules/article-comments/dto/article-comment-basic.dto.ts
+++ src/modules/article-comments/dto/article-comment-basic.dto.ts
@@ -2,7 +2,6 @@ import { OmitType } from '@nestjs/swagger';
 
 import { ArticleComment } from '../entities/article-comment.entity';
 import { BasicUserDto } from '@src/modules/users/dto/user-basic.dto';
-// import { BasicArticleDto } from '@src/modules/articles/dto/article-basic.dto';
 
 export class BasicArticleCommentDto extends OmitType(ArticleComment, [
   'article',
@@ -11,5 +10,4 @@ export class BasicArticleCommentDto extends OmitType(ArticleComment, [
   'children',
 ] as const) {
   user: BasicUserDto;
-  // article: BasicArticleDto;
 }
