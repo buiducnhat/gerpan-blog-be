28cbad45bfd7d19b4504d477f9f1e59fbe353358
gerpan <nhaths4701@gmail.com>
2021-11-14 20:44:40 +0700

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export PATH=~/.nvm/versions/node/v16.13.0/bin:$PATH
-source ~/.profile
-
 # Pull necessary files for preventing errors
 git stash
 git pull origin circleci
@@ -12,4 +9,4 @@ yarn install
 
 # Check if the app was running.
 pm2 delete -s gerpan-blog-be || :
-pm2 start --name gerpan-blog-be npm -- start
+pm2 start --name gerpan-blog-be "yarn start:prod"
