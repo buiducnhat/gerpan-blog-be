38be4180d0f00378fc57f4f64e456222acc051e5
gerpan <nhaths4701@gmail.com>
2021-11-14 22:50:08 +0700

    Config deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Config variable path
-source ~/.profile
 
 # Pull necessary files for preventing errors
 git stash
 git pull origin circleci
 
 # Install dependencies
-yarn install
+`which yarn` install
 
 # Check if the app was running.
-pm2 delete -s gerpan-blog-be || :
-pm2 start --name gerpan-blog-be "yarn start:prod"
+`which pm2` delete -s gerpan-blog-be || :
+`which pm2` start --name gerpan-blog-be "yarn start:prod"
