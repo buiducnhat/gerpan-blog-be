0ca3072334ae890475d80a6ecf1ae9c90d2c4ab1
gerpan <nhaths4701@gmail.com>
2021-11-19 22:36:25 +0700

    Test cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const logger = app.get(Logger);
   const configService: ConfigService<IAppConfig> = app.get(ConfigService);
 
-  app.enableCors();
+  app.enableCors({ origin: /\.vercel\.app$/ });
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(configService.get('apiPrefix'), {
