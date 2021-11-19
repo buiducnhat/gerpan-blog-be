b869bbdfb906d8294b11697ab4e5c1b8be326874
gerpan <nhaths4701@gmail.com>
2021-11-19 23:01:58 +0700

    Test fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, NestApplication } from '@nestjs/core';
 import { Logger, RequestMethod } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as helmet from 'helmet';
+import * as cors from 'cors';
 
 import { AppModule } from './modules/app/app.module';
 import { setupSwagger } from './modules/app/setup-swagger';
@@ -16,7 +17,8 @@ async function bootstrap() {
   const logger = app.get(Logger);
   const configService: ConfigService<IAppConfig> = app.get(ConfigService);
 
-  app.enableCors({ origin: 'https://gerpan.vercel.app' });
+  // app.enableCors();
+  app.use(cors());
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(configService.get('apiPrefix'), {
